8ae042fe596fc3a74e3b2ca49c871509dfde22fe
kr0sette <37877021+kr0sette@users.noreply.github.com>
2018-08-19 13:05:37 -0400

    Working on routing

--- server.js
+++ server.js
@@ -1,24 +1,34 @@
 //Include the following packages
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
 
+
+//====================== SERVER SETUP ====================== 
 //Create an express server
 var app = express();
 
-//set port
-var PORT = process.env.PORT || 8080;
-
-// Sets up the Express app to handle data parsing
+//Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//Set port
+var PORT = process.env.PORT || 8080;
+
+//Start server with a listener
+app.listen(PORT, function(){
+    console.log("App is listening on http://localhost:" + PORT);
+});
+
+//====================== /SERVER SETUP ====================== 
+
+
+//====================== ROUTING SETUP ====================== 
+var apiR = require("./app/routing/apiRoutes");
+var htmlR= require("./app/routing/htmlRoutes");
+
+
+//====================== /ROUTING SETUP ====================== 
 
 //Routing?
 //apiroutes.js
 //htmlroutes.js?
-
-//Start server with a listener
-app.listen(PORT, function(){
-    console.log("App is listening on PORT: " + PORT);
-});

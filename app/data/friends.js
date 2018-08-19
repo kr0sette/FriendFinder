533f5ce0ae1fbea9bb0a85baa885db67ed8caa0c
kr0sette <37877021+kr0sette@users.noreply.github.com>
2018-08-19 14:15:52 -0400

    Added started friends list from demo

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,231 @@
-//Where the friends data/API list will go?
+//Data on all friends are stored here
+//New data on new friends will be pushed to this array
+var friendsList = [
+    {
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "Jacob Deming",
+        "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+        "scores": [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        "name": "Jeremiah Scanlon",
+        "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Louis T. Delia",
+        "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+        "scores": [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    },
+    {
+        "name": "Lou Ritter",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+        "scores": [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        "name": "Jordan Biason",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+        "scores": [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    },
+    {
+        "name": "Jobe K Range",
+        "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjI4sCVv_jcAhWRCHwKHTeMCYgQjRx6BAgBEAU&url=https%3A%2F%2Fwww.pexels.com%2Fphoto%2Fartistic-blossom-bright-clouds-207962%2F&psig=AOvVaw3oJZ6Nchlz7A7S_5S-iXc_&ust=1534746710093310",
+        "scores": [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "2",
+            "1"
+        ]
+    },
+    {
+        "name": "carmen",
+        "photo": "http://carmen.soy/images/me2.png",
+        "scores": [
+            "2",
+            "1",
+            "1",
+            "3",
+            "3",
+            "3",
+            "5",
+            "2",
+            "3",
+            "3"
+        ]
+    },
+    {
+        "name": "jimmy",
+        "photo": "sdlfasdklfasf;",
+        "scores": [
+            "4",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2"
+        ]
+    },
+    {
+        "name": "DUClass",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXgC9r0UOJG4WO8tk6b0YhrOO2XemavTDJmh4lRg7R1_8KRpkj",
+        "scores": [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"
+        ]
+    },
+    {
+        "name": "Eric",
+        "photo": "https://github.com/ekorslin",
+        "scores": [
+            "5",
+            "5",
+            "5",
+            "5",
+            "1",
+            "5",
+            "2",
+            "4",
+            "3",
+            "3"
+        ]
+    },
+    {
+        "name": "Frank",
+        "photo": "www.google.com",
+        "scores": [
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "4",
+            "3",
+            "3",
+            "3",
+            "3"
+        ]
+    },
+    {
+        "name": "Jack Ryan",
+        "photo": "https://vignette.wikia.nocookie.net/rickandmorty/images/0/05/Maximus_Renegade_Starsoldier.jpg/revision/latest?cb=20170814090739",
+        "scores": [
+            "3",
+            "1",
+            "5",
+            "1",
+            "1",
+            "3",
+            "3",
+            "4",
+            "1",
+            "4"
+        ]
+    },
+    {
+        "name": "Vaughn Marques",
+        "photo": "https://static.vinepair.com/wp-content/uploads/2017/06/ind-internal.jpg",
+        "scores": [
+            "2",
+            "2",
+            "3",
+            "3",
+            "4",
+            "3",
+            "2",
+            "2",
+            "4",
+            "2"
+        ]
+    }
+]
+
+//Export the array
+module.exports = friendsList;

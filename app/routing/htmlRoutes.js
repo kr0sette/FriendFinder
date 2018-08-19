0ffd0c06bf155af37ffea4f4fbed5bfa7a72a49f
kr0sette <37877021+kr0sette@users.noreply.github.com>
2018-08-19 19:35:41 -0400

    Created GET request so html files can read Custom CSS file

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -7,12 +7,16 @@ module.exports = function(app){
     //GET function that will direct users to home page
     app.get("/", function(req,res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
-
     });
 
     //GET function that will direct users to survey page
     app.get("/survey", function(req,res){
         res.sendFile(path.join(__dirname, "../public/survey.html"))
+    });
+
+    //Get function that will route to Custom CSS file so that HTML files can read it
+    app.get("/style.css", function(req,res){
+        res.sendFile(path.join(__dirname, "../public/style.css"))
     });
 
 
